76f43d96866e2b20d5df6b5f6a757ad7dd562608
unknown <you@example.com>
2023-01-07 23:52:21 +0000

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Logo from './Logo'
+import FormRow from './FormRow'
+import Alert from './Alert'
+
+export {Logo, FormRow, Alert}
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Error from './Error'
+import { Logo } from '../components';
+
+const Dashboard = () => {
+  return (
+    <h1>
+      <Error />
+      <Logo />
+      Dashboard
+    </h1>
+  )
+}
+export default Dashboard
